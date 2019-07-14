0343a2ec1a986695101ccbd843384db21d6f362c
Cameron Phillips <cameron0505@gmail.com>
2019-07-14 02:05:57 +0100

    Moved parse into its own module and added execute function

--- brainfuck/__init__.py
+++ brainfuck/__init__.py
@@ -1,47 +1,19 @@
 """ A Brainfuck Interpreter """
 
-from typing import List
+
+from brainfuck.parse import parse
+from brainfuck.program import Program
 
 
-class ParseError(Exception):
-    def __init__(self, message):
-        self.message = message
+# The standard cell requirement is 30,000 cells
+CELLS_DEFAULT = 30000
 
 
-def parse(line) -> List:
-
-    ops = []
-    stack = []
-
-    for c in line:
-        if c.isspace():
-            # skip whitespace
-            continue
-        if c in "><+-.,":
-            # add simple operators to the program list
-            ops.append(c)
-        elif c == "[":
-            # save the current program on the stack and start accumulating
-            # instructions until a close loop instruction is encountered
-            stack.append(ops)
-            ops = []
-        elif c == "]":
-            # save the loop body, pop the original program from the stack,
-            # and add the new loop to the end of the program
-            loop = ops
-            if stack:
-                ops = stack.pop()
-            else:
-                raise ParseError("Unexpected ']'")
-            ops.append(loop)
-        else:
-            # report unreckognised character
-            raise ParseError(f"Unknown character '{c}'")
-
-    # report unmatched '['
-    if stack:
-        raise ParseError("Unmatched '['")
-
-    return ops
-
+def execute(string, num_cells=CELLS_DEFAULT):
+    """
+    Parse and evaluate a brainfuck program from a string.
+    """
+    ops = parse(string)
+    program = Program(num_cells)
+    program.execute_many(ops)
 
