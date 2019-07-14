1ff4757f81fd549eb7a628b3589de12382f44d85
Cameron Phillips <cameron0505@gmail.com>
2019-07-14 14:23:13 +0100

    Update execute function to return the program after execution.

--- brainfuck/__init__.py
+++ brainfuck/__init__.py
@@ -17,11 +17,14 @@ CELLS_DEFAULT = 30000
 def execute(string, num_cells=CELLS_DEFAULT):
     """
     Parse and evaluate a brainfuck program from a string.
+    Returns the program after execution.
     """
 
     ops = parse(string)
     program = Program(num_cells)
     program.execute_many(ops)
+
+    return program
 
 
 
